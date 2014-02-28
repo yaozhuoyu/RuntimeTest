d8a4cefda1abd694dbf0150f05e2f6af33f192c1
yaozhuoyu <yaozhuoyu@gmail.com>
2014-02-28 21:37:41 +0800

    初始化工程

---
+++ RuntimeTest/Classes/Animal.h
@@ -0,0 +1,17 @@
+//
+//  Animal.h
+//  RuntimeTest
+//
+//  Created by yaozhuoyu on 14-2-28.
+//  Copyright (c) 2014年 yzy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Animal : NSObject
+
+@property (nonatomic, strong) NSString *anName;
+@property (nonatomic, assign) NSUInteger anAge;
+@property (nonatomic, strong) NSArray *anArray;
+
+@end
---
+++ RuntimeTest/Classes/Animal.m
@@ -0,0 +1,13 @@
+//
+//  Animal.m
+//  RuntimeTest
+//
+//  Created by yaozhuoyu on 14-2-28.
+//  Copyright (c) 2014年 yzy. All rights reserved.
+//
+
+#import "Animal.h"
+
+@implementation Animal
+
+@end
